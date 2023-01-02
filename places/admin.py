42d5bcd5835fe6834e0b0b79e6df43bb620798f0
EgoisTa <97385729+EgoisTa-Git@users.noreply.github.com>
2023-01-02 22:55:39 +0300

    Add Place model

--- places/admin.py
+++ places/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Place
 
-# Register your models here.
+
+@admin.register(Place)
+class AdminPlace(admin.ModelAdmin):
+    list_display = [
+        'title',
+    ]
--- places/models.py
+++ places/models.py
@@ -1,1 +1,26 @@
 from django.db import models
+
+
+class Place(models.Model):
+    title = models.CharField(
+        'Название',
+        max_length=150,
+    )
+    description_short = models.CharField(
+        'Краткое описание',
+        max_length=250,
+    )
+    description_long = models.TextField(
+        'Описание',
+        blank=True,
+    )
+    lng_coordinate = models.DecimalField(
+        'Долгота',
+        max_digits=16,
+        decimal_places=14,
+    )
+    lat_coordinate = models.DecimalField(
+        'Широта',
+        max_digits=16,
+        decimal_places=14,
+    )
