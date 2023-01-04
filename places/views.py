178b72bbd083455651f713d0a2ad66e5f9dc06e5
EgoisTa <97385729+EgoisTa-Git@users.noreply.github.com>
2023-01-04 23:49:46 +0300

    add JSON API Endpoint

--- places/views.py
+++ places/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render, get_object_or_404
 
 from .models import Place
 
@@ -18,9 +19,33 @@ def index(request):
             "properties": {
                 "title": place.title,
                 "placeId": place.pk,
-                "detailsUrl": f"/static/places/{place.pk}.json"
+                "detailsUrl": f"places/{place.pk}/"
             }
         }
         serialized_places.append(place)
     collection = {'type': 'FeatureCollection', 'features': serialized_places}
     return render(request, 'places/index.html', context={"data": collection})
+
+
+def place_detail(request, place_id):
+    place = get_object_or_404(Place, pk=place_id)
+    images = place.images.all()
+    image_urls = []
+    for image in images:
+        image_urls.append(image.image.url)
+    content = {
+        "title": place.title,
+        "imgs": image_urls,
+        "description_short": place.description_short,
+        "description_long": place.description_long,
+        "coordinates": {
+            "lat": place.lat_coordinate,
+            "lng": place.lng_coordinate,
+        },
+    }
+    return JsonResponse(content, safe=True)
+
+
+def place_detail2(request, place_id):
+    place = get_object_or_404(Place, pk=place_id)
+    return HttpResponse(place.title)
