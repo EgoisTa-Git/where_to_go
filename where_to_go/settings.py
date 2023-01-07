f79d2c3056353f108522a2f0977898b784f8d8fe
EgoisTa <97385729+EgoisTa-Git@users.noreply.github.com>
2023-01-07 20:00:22 +0300

    Fix csrf error

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -132,7 +132,3 @@ MEDIA_ROOT = 'media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CSRF_COOKIE_SECURE = True
-
-SESSION_COOKIE_SECURE = True
