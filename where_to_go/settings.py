5d54d4b832cccf2cdec4359273156c197804d90d
EgoisTa <97385729+EgoisTa-Git@users.noreply.github.com>
2023-01-10 21:46:42 +0300

    Remove default secret_key value

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str('SECRET_KEY', 'REPLACE_ME')
+SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', True)
