08fb3495fb0cac5f59ab82169a094ca406f36fdb
EgoisTa <97385729+EgoisTa-Git@users.noreply.github.com>
2023-01-12 21:42:33 +0300

    Add reverse URL dispatcher

--- places/views.py
+++ places/views.py
@@ -1,5 +1,6 @@
 from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
 from .models import Place
 
@@ -19,7 +20,7 @@ def index(request):
             'properties': {
                 'title': place.title,
                 'placeId': place.pk,
-                'detailsUrl': f'places/{place.pk}/'
+                'detailsUrl': reverse('detail', args=(place.pk,))
             }
         }
         serialized_places.append(place)
